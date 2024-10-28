51b44344b07175d33325e4901284bea24bc8dcf2
James Elazegui <jameselazegui21@gmail.com>
2024-10-29 12:30:54 +1300

    Created sideNav
    -created side nav module
    -working on side nav buttons

---
+++ BookNest/BookNest/Modules/SideNavModule.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookNest.Modules
+{
+    /// <summary>
+    /// Interaction logic for SideNavModule.xaml
+    /// </summary>
+    public partial class SideNavModule : UserControl
+    {
+        public SideNavModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
