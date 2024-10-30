dc0c165c0b2b0d92e3764e39b90259b4ede758d3
James Elazegui <jameselazegui21@gmail.com>
2024-10-31 11:28:18 +1300

    Created dropdown component with placeholder as option 1

--- BookNest/BookNest/Components/Dropdown.xaml.cs
+++ BookNest/BookNest/Components/Dropdown.xaml.cs
@@ -15,14 +15,23 @@ using System.Windows.Shapes;
 
 namespace BookNest.Components
 {
-    /// <summary>
-    /// Interaction logic for Dropdown.xaml
-    /// </summary>
     public partial class Dropdown : UserControl
     {
+
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Placeholder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register("Placeholder", typeof(string), typeof(Dropdown), new PropertyMetadata("Placeholder"));
+
         public Dropdown()
         {
             InitializeComponent();
+            DataContext = this;
         }
     }
 }
