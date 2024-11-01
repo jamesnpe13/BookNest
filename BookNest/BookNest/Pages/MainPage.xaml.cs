a3c279bfdd6b0333ce7893d4356263d514497949
James Elazegui <jameselazegui21@gmail.com>
2024-11-01 21:02:17 +1300

    Code refactoring
    Co-Authored-By: jamesnpe14 <186804909+jamesnpe14@users.noreply.github.com>

--- BookNest/BookNest/Pages/MainPage.xaml.cs
+++ BookNest/BookNest/Pages/MainPage.xaml.cs
@@ -1,29 +1,14 @@
 ﻿using BookNest.ViewModels;
-using CommunityToolkit.Mvvm.Messaging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace BookNest.Pages
+namespace BookNest.Pages;
+
+public partial class MainPage : Page
 {
-    public partial class MainPage : Page
+    public MainPage()
     {
-        public MainPage()
-        {
-            InitializeComponent();
-            MainPage_VM vm = new();
-            DataContext = vm;
-        }
+        InitializeComponent();
+        MainPage_VM vm = new();
+        DataContext = vm;
     }
 }
--- BookNest/BookNest/ViewModels/SignInPage_VM.cs
+++ BookNest/BookNest/ViewModels/SignInPage_VM.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BookNest.ViewModels;
 
-namespace BookNest.ViewModels
+class SignInPage_VM
 {
-    class SignInPage_VM
-    {
-    }
 }
