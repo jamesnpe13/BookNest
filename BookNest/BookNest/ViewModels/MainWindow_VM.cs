b7d0587cdb39fc6745512007cef1626327314f1b
James Elazegui <jameselazegui21@gmail.com>
2024-11-12 20:47:06 +1300

    tidy up

--- BookNest/BookNest/ViewModels/MainWindow_VM.cs
+++ BookNest/BookNest/ViewModels/MainWindow_VM.cs
@@ -30,8 +30,6 @@ public partial class MainWindow_VM : ObservableObject
         ss = _ss;
 
         ns.SetCurrentPage(ad.DefaultPage); // sets default page
-
-        ds.DeleteBook("1");
     }
 
     [RelayCommand]
