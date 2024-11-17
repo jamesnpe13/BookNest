23db513960e1e9a4ebc00e39c35c1778e88eb05d
James Elazegui <jameselazegui21@gmail.com>
2024-11-18 12:05:27 +1300

    admin dashboard search functionality functional

--- BookNest/BookNest/Views/Admin_Dashboard_V.xaml.cs
+++ BookNest/BookNest/Views/Admin_Dashboard_V.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BookNest.ViewModels;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace BookNest.Views;
 
@@ -20,4 +21,28 @@ public partial class Admin_Dashboard_V : UserControl
         if (DataContext is MainPage_VM vm) vm.SetCurrentView(targetView);
     }
 
+    private void DashboardSearchField_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            SearchBooks();
+        }
+    }
+
+    private void DashboardSearchButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        SearchBooks();
+    }
+
+    public void SearchBooks()
+    {
+        if (!string.IsNullOrEmpty(DashboardSearchField.Text))
+        {
+            if (DataContext is MainPage_VM vm)
+            {
+                vm.UpdateBookList(BookFilterKey.SEARCH, DashboardSearchField.Text);
+                vm.SetCurrentView(PageView.Books);
+            }
+        }
+    }
 }
