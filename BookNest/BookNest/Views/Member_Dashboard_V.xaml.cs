8b0ba795eac307c5b36b1977966feecab9ec0dba
Boss_Chanonn <chanonjunphansri@gmail.com>
2024-11-18 09:53:10 +1300

    update
    update

--- BookNest/BookNest/Views/Member_Dashboard_V.xaml.cs
+++ BookNest/BookNest/Views/Member_Dashboard_V.xaml.cs
@@ -1,10 +1,25 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace BookNest.Views
 {
+    /// <summary>
+    /// Interaction logic for Member_Dashboard.xaml
+    /// </summary>
     public partial class Member_Dashboard_V : UserControl
     {
-
         public Member_Dashboard_V()
         {
             InitializeComponent();
